2938dc23ecf69324ee05192cde8a19a6159a1ecd
karardataflow <karardataflow@gmail.com>
2024-12-16 17:57:44 +0300

    a

---
+++ view.py
@@ -0,0 +1,5 @@
+import sys 
+
+from PySide6.QtWidgets import QApplication ,QMainWindow , QPushButton, QGridLayout, QFrame ,QLabel
+from PySide6.QtGui import QIcon , QAction ,QPixmap
+from PySide6.QtCore import Qt
